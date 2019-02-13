64e3c8e7ce3d2597015cede10acb4b340fa7afec
soulstrk <soulstrk1234@gmail.com>
2019-02-13 19:45:25 +0900

    백준 단계별 함수사용파트

--- 알고리즘/src/백준문제/Back1065.java
+++ 알고리즘/src/백준문제/Back1065.java
@@ -25,24 +25,52 @@ public class Back1065 {
 		
 		for (int i = 100; i <= n; i++) {
 			
-			arr[0] = i/100; // 1
-			arr[1] = i/10 - arr[0]*10; // 2
-			if(i == 123) {
-				System.out.println(arr[0]);
-				System.out.println(arr[1]);
-				System.out.println(i);
-			}
-			arr[2] = i - (arr[0]*100) + (arr[1]*10); // 3
-			if(i == 123) {
-				System.out.println(arr[2]);
-			}
-			
+			arr[0] = i/100; 
+			arr[1] = i/10 - arr[0]*10; 
+			arr[2] = i - (arr[0]*100) - (arr[1]*10); 
 			
 			if (arr[0] - arr[1] == arr[1] - arr[2]) {
 				sum ++;
 			}
 		}
 		
-		
+		System.out.println(sum);
 	}
 }
+
+
+/*
+ * public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+         
+        if (n < 100) {
+            System.out.println(n);
+        } else {
+            int result = 99;
+             
+            for (int i = 100; i <= n; ++i) {
+                result += checkHanNumber(i);
+            }
+             
+            if (n == 1000) result--;
+             
+            System.out.println(result);
+        }
+    }
+     
+    private static int checkHanNumber(int number) {
+        int num1 = number / 100 % 10;
+        int num2 = number / 10 % 10;
+        int num3 = number % 10;
+         
+        if (num2 * 2 == num1 + num3) {
+            return 1;
+        }
+        return 0;
+    }
+}
+*/
